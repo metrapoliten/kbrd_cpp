fd9e24bdcebda2f140fd284930a7750eb7550005
Artyom Gavrilov <artyom.e.gavrilov@gmail.com>
2026-03-01 17:37:17 +0300

    Add tests

--- test/FakeHidapi.cpp
+++ test/FakeHidapi.cpp
@@ -2,23 +2,26 @@
 
 #include "MockHidApi.hh"
 
-MockHidApi* mockHid = nullptr;
+MockHidApi *mockHid = nullptr;
 
 extern "C" {
-  int hid_init() { return mockHid ? mockHid->hid_init() : -1; }
-  hid_device* hid_open(unsigned short vendor_id, unsigned short product_id, const wchar_t* serial_number) {
-    return mockHid ? mockHid->hid_open(vendor_id, product_id, serial_number) : nullptr;
-  }
-  int hid_send_feature_report(hid_device* dev, const unsigned char* data, size_t length) {
-    return mockHid ? mockHid->hid_send_feature_report(dev, data, length) : -1;
-  }
-  int hid_get_feature_report(hid_device* dev, unsigned char* data, size_t length) {
-    return mockHid ? mockHid->hid_get_feature_report(dev, data, length) : -1;
-  }
-  const wchar_t* hid_error(hid_device* dev) {
-    return mockHid ? mockHid->hid_error(dev) : L"Mock Error";
-  }
-  void hid_close([[maybe_unused]]hid_device* dev) {}
-  int hid_exit() { return mockHid ? mockHid->hid_exit() : -1; }
+int hid_init() { return mockHid ? mockHid->hid_init() : -1; }
+hid_device *hid_open(unsigned short vendor_id, unsigned short product_id,
+                     const wchar_t *serial_number) {
+  return mockHid ? mockHid->hid_open(vendor_id, product_id, serial_number)
+                 : nullptr;
 }
-
+int hid_send_feature_report(hid_device *dev, const unsigned char *data,
+                            size_t length) {
+  return mockHid ? mockHid->hid_send_feature_report(dev, data, length) : -1;
+}
+int hid_get_feature_report(hid_device *dev, unsigned char *data,
+                           size_t length) {
+  return mockHid ? mockHid->hid_get_feature_report(dev, data, length) : -1;
+}
+const wchar_t *hid_error(hid_device *dev) {
+  return mockHid ? mockHid->hid_error(dev) : L"Mock Error";
+}
+void hid_close([[maybe_unused]] hid_device *dev) {}
+int hid_exit() { return mockHid ? mockHid->hid_exit() : -1; }
+}
